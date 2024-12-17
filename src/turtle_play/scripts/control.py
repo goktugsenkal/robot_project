dbcbb3533e8014eee14d3b00da558b72bbe5cab1
Goktug Ubuntu 20.04 <goktugsenkal1881@gmail.com>
2024-12-17 21:01:50 +0300

    add turtle r&d file

---
+++ src/turtle_play/scripts/control.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import rospy
+from geometry_msgs.msg import Twist
+from turtlesim.msg import Pose
+import random
+
+rospy.init_node("control")
+
+pub = rospy.Publisher("/turtle1/cmd_vel", Twist, queue_size=1)
+
+def callback(msg):
+    print(msg)
+
+rospy.Subscriber("/turtle1/pose", Pose, callback)
+
+msg = Twist()
+
+while not rospy.is_shutdown():
+    msg.angular.z = random.randint(0,20)
+    msg.angular.y = random.randint(0,20)
+    msg.linear.x = random.randint(0,20)
+    msg.linear.y = random.randint(0,20)
+    
+    pub.publish(msg)
+    rospy.sleep(0.1)
+
+rospy.spin()
