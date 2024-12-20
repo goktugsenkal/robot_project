a76c48beb668a930bc0b99218f5d51bddf86b583
Goktug Ubuntu 20.04 <goktugsenkal1881@gmail.com>
2024-12-20 13:04:46 +0300

    add 2 publishers & subs

--- src/pub_sub/scripts/publisher.py
+++ src/pub_sub/scripts/publisher.py
@@ -5,10 +5,12 @@ from std_msgs.msg import Int8, Char, Float32, String, UInt64MultiArray
 
 # node & topic init
 rospy.init_node("publisher_node")
-pub = rospy.Publisher("topic", String, queue_size=1)
+pub = rospy.Publisher("test", String, queue_size=1)
+pub_2 = rospy.Publisher("test_2", String, queue_size=1)
 
 # publish loop
 while not rospy.is_shutdown():
-    pub.publish("wenk" )
+    pub.publish("wenk")
+    pub_2.publish("wenk 2")
     rospy.sleep(1)
 
--- src/pub_sub/scripts/subscriber.py
+++ src/pub_sub/scripts/subscriber.py
@@ -9,6 +9,7 @@ def topic_callback(msg):
 
 rospy.init_node("subscriber_node")
 
-sub = rospy.Subscriber("topic", String, callback=topic_callback)
 
+sub = rospy.Subscriber("test", String, callback=topic_callback)
+sub_2 = rospy.Subscriber("test_2", String, callback=topic_callback)
 rospy.spin()
